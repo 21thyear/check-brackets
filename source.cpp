81509c9f12ceafe41538d986fb8c6b2d3e66ff79
Роман <135020641+21thyear@users.noreply.github.com>
2023-10-02 22:38:52 +0300

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+bool checkBrackets(const std::string& expression)
+{
+    std::stack<char> brackets;
+
+    int openedBracketsCount = 0;
+    int closedBracketsCount = 0;
+
+    for (char c : expression) {
+        if (c == '(' || c == '{' || c == '[') {
+            openedBracketsCount++;
+            brackets.push(c);
+        }
+        else if (c == ')' || c == '}' || c == ']') {
+            closedBracketsCount++;
+            if (brackets.empty()) {
+                return false;
+            }
+            char top = brackets.top();
+            brackets.pop();
+            if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+
+    //std::cout << "Opened: " + openedBracketsCount << std::endl;
+    //std::cout << "Closed: " + closedBracketsCount << std::endl;
+
+    return brackets.empty();
+}
+int main()
+{
+    std::string brackets = "{}}}{[{{}{}{}{{{]}}}][";
+    if (checkBrackets(brackets))
+    {
+        std::cout << "All brackets - good" << std::endl;
+        return true;
+    }
+    std::cout << "Bad brackets :(" << std::endl;
+    return false;
+}
